aa0f72ee50e61f43a98c79849f90bb3892764256
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-09-01 21:37:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const td01 = document.createElement("td");
 tr0.style.columnSpan = "2";
 tr0.innerText = "Loading...";
 // tr0.append(td0,td01)
-body.append(tr0);
+// body.append(tr0);
 
 let p1 = new Promise((resolve) =>{
 	setTimeout(() =>{
@@ -31,7 +31,7 @@ let arr = [p1,p2,p3];
 let all = Promise.all(arr);
 
 all.then((data) =>{
-	tr0.style.display = "none";
+	// tr0.style.display = "none";
 	const td1 = document.createElement("td");
 	const td2 = document.createElement("td");
 	const r1 = document.createElement("tr");
